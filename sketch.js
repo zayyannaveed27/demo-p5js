314609a80ae6eca411fc74d85024c6e1adef229a
tonywang25 <90213843+tonywang25@users.noreply.github.com>
2023-04-06 14:11:23 -0400

    final

--- sketch.js
+++ sketch.js
@@ -116,10 +116,10 @@ function setup() {
 }
 
 function draw() {
-  background('#b1d1fc');
+  background('#000000');
   for (let i = 0; i < numSegments - 1; i++) {
     line(xCor[i], yCor[i], xCor[i + 1], yCor[i + 1]);
-    stroke('#c1f80a');
+    stroke('#FFFF00');
   }
   updateSnakeCoordinates();
   updateHandContainer();
